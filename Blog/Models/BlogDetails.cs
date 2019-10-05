16848a3e6a65c7fad14887c158cf91a662033633
Sai <venkat7sai@outlook.com>
2019-10-05 13:15:08 -0400

    Added DAL Layer, and Added class.

---
+++ Blog/Models/BlogDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class BlogDetails
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ DataAccess/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public DateTime? BlogDate { get; set; }
+        public string BlogDescription { get; set; }
+        public ICollection<Comments> Comments { get; set; }
+
+
+    }
+}
